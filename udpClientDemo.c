2d71aa298c083cacff80ea7ad1be203a96bb8d2e
YangToW <wuyang199911@163.com>
2021-01-18 18:27:18 +0800

    update udpClientDemo.c

--- udpClientDemo.c
+++ udpClientDemo.c
@@ -4,15 +4,16 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-const int BUFLEN 2048
-const int MSG_NUM 5
-const int SERVICE_PORT 6240
-const char * const SERVICE_IP "127.0.0.1"
+const int BUFLEN = 2048;
+const int MSG_NUM = 5;
+const int SERVICE_PORT = 6240;
+const char * const SERVICE_IP = "127.0.0.1";
 
 int main(int argc, char **argv)
 {
@@ -23,7 +24,7 @@ int main(int argc, char **argv)
         perror("cannot create socket");
 		exit(0);
     }
-	printf("created socket, fd: %d\n", fd);
+	printf("created socket, fd: %d\n", sockCli);
 	// 命名套接字，在客户端这是非必须的
 	// 对套接字命名的意思是指将该套接字关联一个IP地址和端口号，可以使用系统调用bind来实现。
 	struct sockaddr_in cliAddr;
@@ -33,7 +34,7 @@ int main(int argc, char **argv)
 	cliAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	// cliAddr.sin_addr.s_addr = inet_addr("127.0.0.1")
 	
-	if (bind(sockCli，(struct sockaddr *)&cliAddr, sizeof(cliAddr)) < 0)
+	if (bind(sockCli, (struct sockaddr *)&cliAddr, sizeof(cliAddr)) < 0)
 	{
 		perror("bind failed");
 		exit(0);
@@ -52,11 +53,12 @@ int main(int argc, char **argv)
 	// 客户向服务器发送消息
 	char buf[BUFLEN];
 	int slen = sizeof(servAddr);
-	for (i = 0; i < MSG_NUM; i++)
+	int sendLen;
+	for (int i = 0; i < MSG_NUM; i++)
 	{
 		printf("Sending packet %d to %s port %d\n", i, SERVICE_IP, SERVICE_PORT);
 		sprintf(buf, "This is packet %d", i);
-		sendLen = send(sockCli, buf, strlen(buf), 0, (struct sockaddr *)&servAddr, slen);
+		sendLen = sendto(sockCli, buf, strlen(buf), 0, (struct sockaddr *)&servAddr, slen);
 		if (sendLen == -1)
 		{
 			perror("sendto");
